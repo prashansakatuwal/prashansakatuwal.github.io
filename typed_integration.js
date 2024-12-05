a52b1fff60b21f6ed9dafb83ddd1933ea7e2f213
prashansakatuwal <68369753+prashansakatuwal@users.noreply.github.com>
2024-12-05 23:01:41 +1100

    new look

---
+++ typed_integration.js
@@ -0,0 +1,12 @@
+
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".typed-text", {
+      strings: ["Software Developer", "Web Designer", "Innovator"],
+      typeSpeed: 60,
+      backSpeed: 30,
+      loop: true
+    });
+  });
+</script>
